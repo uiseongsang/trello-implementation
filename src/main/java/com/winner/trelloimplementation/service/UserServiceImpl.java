8ce20203b9cfc2380092e737c2316e38d5be1d6a
shung1103 <shung1103@hanmail.net>
2023-08-07 16:13:39 +0900

    기본 로그인 기능과 spring security 추가

---
+++ src/main/java/com/winner/trelloimplementation/service/UserServiceImpl.java
@@ -0,0 +1,44 @@
+package com.winner.trelloimplementation.service;
+
+import com.winner.trelloimplementation.dto.ApiResponseDto;
+import com.winner.trelloimplementation.dto.SignupRequestDto;
+import com.winner.trelloimplementation.entity.User;
+import com.winner.trelloimplementation.entity.UserRoleEnum;
+import com.winner.trelloimplementation.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public ResponseEntity<ApiResponseDto> signup(SignupRequestDto requestDto) {
+        if (requestDto.getPassword().contains(requestDto.getUsername())) {
+            throw new IllegalArgumentException("비밀번호에 닉네임이 포함되어 있습니다.");
+        }
+        String username = requestDto.getUsername();
+        String nickname = requestDto.getNickname();
+        String password = passwordEncoder.encode(requestDto.getPassword());
+        String email = requestDto.getEmail();
+        String introduction = requestDto.getIntroduction();
+        UserRoleEnum role = UserRoleEnum.USER;
+
+        // 회원 중복 확인
+        if (userRepository.findByUsername(username).isPresent()) {
+            throw new IllegalArgumentException("중복된 사용자가 존재합니다.");
+        }
+
+        // 사용자 등록
+        User user = new User(username, nickname, password, email, introduction, role);
+        userRepository.save(user);
+        return ResponseEntity.status(201).body(new ApiResponseDto("회원가입 성공", HttpStatus.CREATED.value()));
+    }
+}
