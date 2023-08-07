0b31993f7d418560f5cd756f815f948a6fbbac0e
shung1103 <shung1103@hanmail.net>
2023-08-07 16:36:54 +0900

    기본 로그인 기능과 spring security 추가

---
+++ src/main/java/com/winner/trelloimplementation/user/entity/User.java
@@ -0,0 +1,48 @@
+package com.winner.trelloimplementation.user.entity;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "users")
+@EqualsAndHashCode
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "nickname", nullable = false, unique = true)
+    private String nickname;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "introduction")
+    private String introduction;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum role;
+
+    public User(String username, String nickname, String password, String email, String introduction, UserRoleEnum role) {
+        this.username = username;
+        this.nickname = nickname;
+        this.password = password;
+        this.email = email;
+        this.introduction = introduction;
+        this.role = role;
+    }
+}
