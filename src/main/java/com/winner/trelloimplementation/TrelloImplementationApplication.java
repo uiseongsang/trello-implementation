ecfd23fbd8caf4b1b4ee6129db1bf909dcaacd54
Thomas Sang <40707686+uiseongsang@users.noreply.github.com>
2023-08-07 14:39:50 +0900

    init

---
+++ src/main/java/com/winner/trelloimplementation/TrelloImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.winner.trelloimplementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloImplementationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrelloImplementationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/winner/trelloimplementation/TrelloImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winner.trelloimplementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloImplementationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
