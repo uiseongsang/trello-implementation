fda26697fd623c5316d2063ffb1743c47fde8de9
shung1103 <shung1103@hanmail.net>
2023-08-07 17:11:50 +0900

    Requestbody 추가

--- src/main/java/com/winner/trelloimplementation/user/controller/UserController.java
+++ src/main/java/com/winner/trelloimplementation/user/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,7 +32,7 @@ public class UserController {
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<ApiResponseDto> signup(@Valid SignupRequestDto requestDto) {
+    public ResponseEntity<ApiResponseDto> signup(@Valid @RequestBody SignupRequestDto requestDto) {
         return userServiceImpl.signup(requestDto);
     }
 }
