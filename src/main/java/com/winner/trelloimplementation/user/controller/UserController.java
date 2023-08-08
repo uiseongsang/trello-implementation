2aec7d4549b78270dae9d6340d61ceed01fd490b
shung1103 <shung1103@hanmail.net>
2023-08-08 18:18:18 +0900

    카카오 사용자 정보 가져오기

--- src/main/java/com/winner/trelloimplementation/user/controller/UserController.java
+++ src/main/java/com/winner/trelloimplementation/user/controller/UserController.java
@@ -1,11 +1,20 @@
 package com.winner.trelloimplementation.user.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.winner.trelloimplementation.common.dto.ApiResponseDto;
+import com.winner.trelloimplementation.common.jwt.JwtUtil;
 import com.winner.trelloimplementation.common.security.JwtAuthenticationFilter;
 import com.winner.trelloimplementation.common.security.UserDetailsImpl;
 import com.winner.trelloimplementation.user.dto.*;
+import com.winner.trelloimplementation.user.service.KakaoService;
 import com.winner.trelloimplementation.user.service.UserServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -24,39 +33,83 @@ import java.io.IOException;
 public class UserController {
     private final UserServiceImpl userServiceImpl;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final KakaoService kakaoService;
 
+    @Operation(summary = "로그아웃 메서드", description = "JWT 쿠키를 삭제하여 로그아웃 합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(implementation = ApiResponseDto.class))),
+            @ApiResponse(responseCode = "400", description = "bad request operation", content = @Content(schema = @Schema(implementation = ApiResponseDto.class)))
+    })
     @GetMapping("/logout")
     public ResponseEntity<ApiResponseDto> logout(HttpServletResponse response, Authentication authResult) throws ServletException, IOException {
         jwtAuthenticationFilter.deleteAuthentication(response, authResult);
         return ResponseEntity.status(200).body(new ApiResponseDto("로그아웃 성공", HttpStatus.OK.value()));
     }
 
+    @Operation(summary = "회원가입 메서드", description = "회원가입 메서드입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "successful operation", content = @Content(schema = @Schema(implementation = ApiResponseDto.class))),
+            @ApiResponse(responseCode = "400", description = "bad request operation", content = @Content(schema = @Schema(implementation = ApiResponseDto.class)))
+    })
     @PostMapping("/signup")
     public ResponseEntity<ApiResponseDto> signup(@Valid @RequestBody SignupRequestDto requestDto) {
         return userServiceImpl.signup(requestDto);
     }
 
+    @Operation(summary = "유저 프로필 조회 메서드", description = "유저 프로필 조회 메서드입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(implementation = ProfileResponseDto.class))),
+            @ApiResponse(responseCode = "400", description = "bad request operation", content = @Content(schema = @Schema(implementation = ProfileResponseDto.class)))
+    })
     @GetMapping("/info")
     public ProfileResponseDto getProfile(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         return userServiceImpl.getProfile(userDetails.getUser());
     }
 
+    @Operation(summary = "유저 프로필 수정 메서드", description = "유저 프로필 수정 메서드입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(implementation = ProfileResponseDto.class))),
+            @ApiResponse(responseCode = "400", description = "bad request operation", content = @Content(schema = @Schema(implementation = ProfileResponseDto.class)))
+    })
     @Transactional
     @PutMapping("/info")
     public ResponseEntity<ApiResponseDto> updateProfile(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody ProfileRequestDto profileRequestDto) {
         return userServiceImpl.updateProfile(userDetails.getUser(), profileRequestDto);
     }
 
+    @Operation(summary = "비밀번호 수정 메서드", description = "현재 비밀번호를 확인하고 새로운 비밀번호로 수정하는 메서드입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(implementation = PasswordRequestDto.class))),
+            @ApiResponse(responseCode = "400", description = "bad request operation", content = @Content(schema = @Schema(implementation = PasswordRequestDto.class)))
+    })
     @Transactional
     @PutMapping("/info/password")
     public ResponseEntity<ApiResponseDto> updatePassword(@AuthenticationPrincipal UserDetailsImpl userDetails, @Valid @RequestBody PasswordRequestDto passwordRequestDto) {
         return userServiceImpl.updatePassword(userDetails.getUser(), passwordRequestDto);
     }
 
+    @Operation(summary = "회원탈퇴 메서드", description = "회원탈퇴 메서드입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "successful operation", content = @Content(schema = @Schema(implementation = ApiResponseDto.class))),
+            @ApiResponse(responseCode = "400", description = "bad request operation", content = @Content(schema = @Schema(implementation = ApiResponseDto.class)))
+    })
     @Transactional
     @DeleteMapping("/signout")
     public ResponseEntity<ApiResponseDto> signout(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody SignoutRequestDto signoutRequestDto,
                                                   HttpServletResponse response, Authentication authResult) throws ServletException, IOException {
         return userServiceImpl.signout(userDetails.getUser(), signoutRequestDto, response, authResult);
+    }
+
+    @GetMapping("/user/kakao/callback")
+    public String kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
+        // code: 카카오 서버로부터 받은 인가 코드 Service 전달 후 인증 처리 및 JWT 반환
+        String token = kakaoService.kakaoLogin(code);
+
+        // Cookie 생성 및 직접 브라우저에 Set
+        Cookie cookie = new Cookie(JwtUtil.AUTHORIZATION_HEADER, token.substring(7));
+        cookie.setPath("/");
+        response.addCookie(cookie);
+
+        return "redirect:/";
     }
 }
