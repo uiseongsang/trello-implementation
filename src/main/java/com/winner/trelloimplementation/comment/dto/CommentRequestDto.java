b383a21500e062b8df605c145415aaa1398a7396
youngmin13 <wjsdudals56@naver.com>
2023-08-10 12:38:03 +0900

    오타 수정 및 Builder & NoArgsContructor 추가

--- src/main/java/com/winner/trelloimplementation/comment/dto/CommentRequestDto.java
+++ src/main/java/com/winner/trelloimplementation/comment/dto/CommentRequestDto.java
@@ -1,8 +1,16 @@
 package com.winner.trelloimplementation.comment.dto;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class CommentRequestDto {
     private String content;
+
+    @Builder
+    public CommentRequestDto (String content) {
+        this.content = content;
+    }
 }
