e07150e4428de4dba628ef0a17fcf351158114f4
shung1103 <shung1103@hanmail.net>
2023-08-10 11:07:42 +0900

    로그 저장 기능

--- src/main/java/com/winner/trelloimplementation/user/dto/LoginRequestDto.java
+++ src/main/java/com/winner/trelloimplementation/user/dto/LoginRequestDto.java
@@ -1,5 +1,6 @@
 package com.winner.trelloimplementation.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.winner.trelloimplementation.user.entity.UserRoleEnum;
 import lombok.Data;
 
@@ -7,5 +8,6 @@ import lombok.Data;
 public class LoginRequestDto {
     private String username;
     private String password;
+    @JsonIgnore
     private UserRoleEnum role;
 }
